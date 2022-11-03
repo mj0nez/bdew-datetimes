0055df56b9e6a633c2535c3948385c49e69d7195
mj0nez <mj0nez@fn.de>
2022-11-03 16:15:14 +0100

    separated bdew holidays and german holidays, cleaned docstr

--- energy_datetime/calendar.py
+++ energy_datetime/calendar.py
@@ -1,46 +1,42 @@
 from datetime import date
 
-import holidays
+from holidays import HolidayBase, HolidaySum
 from holidays.constants import DEC
 from holidays.countries.germany import Germany
 
 
-class BdewHolidays(Germany):
-    """Special holidays for the BDEW, excluding wide nation oder country specific holidays.
+class BdewDefinedHolidays(HolidayBase):
+    """Special holidays considered by the BDEW, excluding nation wide or state specific holidays.
 
     To get a full calendar with all holidays use the function `create_bdew_calendar`.
 
-    Currently, in Germany there are no observed holidays e.g. a Holiday is moved to the next Workday, if it's on Sunday.
+    Currently, in Germany there are no observed holidays e.g. a Holiday is
+    moved to the next Workday, if it's on Sunday.
 
-    Nach GPKE und GeLi Gas sind alle jene Tage Werktage, die weder Sonnabend, Sonntag oder gesetzlicher Feiertag sind.
-    Generell gelten der 24.12. und
-    der 31.12. als Feiertage. Angaben ohne Gewähr.
-
-    https://www.bdew.de/media/documents/GPKE-GeLiGas-Feiertagskalender-2022-Version-1-1_Uu1w6UQ.pdf
-
-    [Accessed 2022-11-02]
-
-    Parameters
-    ----------
-    Germany : _type_
-        _description_
     """
 
     def __init__(self, observed: bool = False, **kwargs):
-        kwargs.update({"observed": observed})
-        super().__init__(**kwargs)
+        super().__init__(self, observed=observed, **kwargs)
 
     def _populate(self, year):
-        super()._populate(year)
 
-        #
         self[date(year, DEC, 24)] = "Heiligabend"
         self[date(year, DEC, 31)] = "Silvester"
 
 
-def create_bdew_calendar() -> holidays.HolidaySum:
-    """Creates a calendar containing the holidays used by the BDEW.
+def create_bdew_calendar() -> HolidaySum:
+    """Creates a calendar containing all days considered by the BDEW.
 
+    Currently, in Germany there are no observed holidays e.g. a Holiday is
+    moved to the next Workday, if it's on Sunday.
+
+    "Nach GPKE und GeLi Gas sind alle jene Tage Werktage, die weder Sonnabend,
+    Sonntag oder gesetzlicher Feiertag sind. Generell gelten der 24.12. und
+    der 31.12. als Feiertage. Angaben ohne Gewähr."
+
+    https://www.bdew.de/media/documents/GPKE-GeLiGas-Feiertagskalender-2022-Version-1-1_Uu1w6UQ.pdf
+
+    [Accessed 2022-11-02]``
 
     Returns
     -------
@@ -49,12 +45,12 @@ def create_bdew_calendar() -> holidays.HolidaySum:
     """
 
     # First we need the BDEW specific holidays.
-    calendar = BdewHolidays()
+    calendar = BdewDefinedHolidays()
 
     # If a day is holiday in any subdivision, the holiday is valid nationwide.
     # Therefore we add all subdivisions of Germany to the BDEW specific holidays.
     # Currently, in Germany holidays are not observed.
-    for cal in calendar.subdivisions:
+    for cal in Germany.subdivisions:
         calendar += Germany(subdiv=cal, observed=False)
 
     return calendar
--- tests/test_calendar.py
+++ tests/test_calendar.py
@@ -1,15 +1,13 @@
-from energy_datetime.calendar import BdewHolidays, create_bdew_calendar
+from energy_datetime.calendar import BdewDefinedHolidays, create_bdew_calendar
 
 from datetime import date, datetime
 
 
 def test_bdew_holidays():
-    calendar = BdewHolidays()
+    calendar = BdewDefinedHolidays()
 
     assert date(2021, 12, 31) in calendar  # ensure silvester is valid
-
-    assert date(2022, 1, 1) in calendar
-    assert date(2022, 1, 2) not in calendar
+    assert date(2021, 12, 24) in calendar  # ensure silvester is valid
 
 
 def test_create_bdew_calendar():
@@ -40,8 +38,9 @@ def test_holiday_check():
     assert "2022-01-01" in calendar
     assert "1/1/2022" in calendar
 
-    ts = datetime(2022, 1, 1, 22, 16, 59).timestamp()  # POSIX timestamp: 1641071819.0
+    ts = datetime(
+        2022, 1, 1, 22, 16, 59
+    ).timestamp()  # POSIX timestamp: 1641071819.0
 
     assert ts in calendar
     assert int(ts) in calendar
-

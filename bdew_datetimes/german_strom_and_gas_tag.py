2e6e8d0bbd62b6970f86f59106f1cbba0975048b
konstantin <konstantin.klein@hochfrequenz.de>
2023-01-26 09:11:04 +0100

    Add functions to differentiate German Strom- und Gastag
    These are mostly copied from `ahbicht` where they are used to evaluate `[UB1]`, `[UB2]`, `[UB2]`
    https://github.com/Hochfrequenz/ahbicht/blob/main/src/ahbicht/content_evaluation/german_strom_and_gas_tag.py

---
+++ bdew_datetimes/german_strom_and_gas_tag.py
@@ -0,0 +1,95 @@
+"""
+A module to evaluate datetimes and whether they are "on the edge"
+of a German "Stromtag" or "Gastag" respectively
+"""
+from datetime import datetime, time
+from typing import Callable, Literal, Union
+
+# The problem with the stdlib zoneinfo is, that the availability of timezones
+# via ZoneInfo(zone_key) depends on the OS and system on which you're running
+# it. In some cases "Europe/Berlin" might be available, but generally it's not,
+# and it's PITA to manually define timezones. So we're using pytz as a
+# datasource for timezone information.
+from pytz import timezone, utc
+
+berlin = timezone("Europe/Berlin")
+
+
+def _get_german_local_time(date_time: datetime) -> time:
+    """
+    returns the current german local time for the given datetime object
+    """
+    german_local_datetime = date_time.astimezone(berlin)
+    return german_local_datetime.time()
+
+
+def has_no_utc_offset(date_time: datetime) -> bool:
+    """
+    returns true if and only if date_time has an explicit offset
+    which and the UTC-offset is 0. This means the UTC offset is exactly "+00:00".
+    """
+    # the name of the function contains a negation because in German
+    # market communication it often matters that the UTC offset is 0.
+    original_time = date_time.time()
+    utc_time = date_time.astimezone(tz=utc).time()
+    return (
+        utc_time == original_time
+        and utc_time.hour == 0
+        and utc_time.minute == 0
+        and utc_time.second == 0
+    )
+
+
+def is_stromtag_limit(
+    date_time: datetime,
+) -> bool:  # the name is not as speaking as I'd like it to be
+    """
+    Returns true if and only if the given date_time is the inclusive
+    start or exclusive end of a german "Stromtag". The "Stromtag" is
+    the balancing relevant day in the German electricity market.
+    It starts and ends at midnight in German local time which can be
+    either 23PM or 22PM in UTC (depending on the daylight saving time in Germany).
+    """
+    german_local_time = _get_german_local_time(date_time)
+    return (
+        german_local_time.hour == 0
+        and german_local_time.minute == 0
+        and german_local_time.second == 0
+    )
+
+
+def is_gastag_limit(
+    date_time: datetime,
+) -> bool:  # the name is not as speaking as I'd like it to be
+    """
+    Returns true if and only if the given date_time is the
+    inclusive start or exclusive end of a german "Gastag".
+    The "Gastag" is the balancing relevant day in the German gas market.
+    It starts and ends at 6am in German local time which can be either
+    4AM or 5AM in UTC (depending on the daylight saving time in Germany).
+    """
+    german_local_time = _get_german_local_time(date_time)
+    return (
+        german_local_time.hour == 6
+        and german_local_time.minute == 0
+        and german_local_time.second == 0
+    )
+
+
+def is_xtag_limit(
+    date_time: datetime, division: Union[Literal["Strom"], Literal["Gas"]]
+) -> bool:
+    """
+    Tries to parse the entered_input as datetime and checks if it is the
+    start/end of a Strom- or Gastag
+    """
+    xtag_evaluator: Callable[[datetime], bool]
+    if division == "Strom":
+        xtag_evaluator = is_stromtag_limit
+    elif division == "Gas":
+        xtag_evaluator = is_gastag_limit
+    else:
+        raise NotImplementedError(
+            f"The division must either be 'Strom' or 'Gas': '{division}'"
+        )
+    return xtag_evaluator(date_time)

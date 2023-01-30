a6bcdb05efdf6c5f24ff9605b7362a21438c6abe
konstantin <konstantin.klein@hochfrequenz.de>
2023-01-30 11:56:04 +0100

    🚚 Move `GERMAN_TIME_ZONE` to `__init__.py`
    This makes it easier to import it from different modules within package (wihout creating circular import loops)
    remove unused import

--- src/bdew_datetimes/__init__.py
+++ src/bdew_datetimes/__init__.py
@@ -1,6 +1,10 @@
 """
 bdew_datetimes is a package that models the BDEW holiday, which is relevant for German utilities
 """
+from pytz import timezone
+
 from .calendar import create_bdew_calendar
+
+GERMAN_TIME_ZONE = timezone("Europe/Berlin")
 
 __version__ = "0.2.0"
